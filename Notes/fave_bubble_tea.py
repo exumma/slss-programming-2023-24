6260b3b784fa159f4080c3d8c58a74993e61696a
Emma Xu <emmazihanxu@gmail.com>
2023-10-30 08:29:19 -0700

    finished fave bubble tea bot

---
+++ Notes/fave_bubble_tea.py
@@ -0,0 +1,45 @@
+# Bubble Tea Popularity Algorithm
+# Author: Emma
+# Date: 27 October 2023
+
+# Ask 5 users what their favourite bubble tea place is 
+# Print out a summary of the data
+
+# ------ CONSTANTS
+
+NUM_RESPONDENTS = 5
+
+# ------
+
+# Like counters
+coco_likes = 0          # Initialize the variable to 0
+suntea_likes = 0
+chatime_likes = 0
+bubqueen_likes = 0
+
+for _ in range(NUM_RESPONDENTS):
+    # Ask the user what their favourite place is
+    # Store their information somewhere
+    print("What's your favourite bubble tea place?")
+    fave_place = input().strip(".,!? ").lower()
+
+    # Tally or counting algo
+    # If they choose any of the options above, increase the counter
+    if fave_place == "coco":
+        coco_likes = coco_likes + 1
+    elif fave_place == "suntea":
+        suntea_likes += 1           # alternative to above
+    elif fave_place == "chatime":
+        chatime_likes += 1
+    elif fave_place == "bubble queen":
+        bubqueen_likes += 1
+
+# Repeat the code above 5 times
+
+# Print out a summary
+# Give the raw score AND the percentage
+print(f"CoCo Likes: {coco_likes}: {coco_likes/NUM_RESPONDENTS*100}%")
+print(f"Suntea Likes: {suntea_likes}: {suntea_likes/NUM_RESPONDENTS*100}%")
+print(f"Chatime Likes: {chatime_likes}: {chatime_likes/NUM_RESPONDENTS*100}%")
+print(f"Bubble Queen Likes: {bubqueen_likes}: {bubqueen_likes/NUM_RESPONDENTS*100}%")
+
---
+++ Notes/number_practice.py
@@ -0,0 +1,16 @@
+# Number Practice
+# Emma
+# 26 October 2023
+
+# Operators that work on Numbers 
+print(1+1)
+print(1-1)
+print(10*10)       # Multiply
+print(9/3)         # Division
+
+print(1+2/4+5-4)   # BEDMAS
+
+print(9//2)        # Floor Division
+print(9%2)         # Modulo gives remainder
+
+print(2**10)       # Power
