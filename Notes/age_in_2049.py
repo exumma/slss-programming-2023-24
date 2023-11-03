72d133b93b104fcee6408f6f06ccfbb22ece9eb1
Emma Xu <emmazihanxu@gmail.com>
2023-11-03 08:58:20 -0700

    added age in 2049

---
+++ Notes/age_in_2049.py
@@ -0,0 +1,14 @@
+# Age in 2049
+# Emma Xu
+# 1 November 2023
+
+# Ask how old the user is
+user_age = int(input("How old are you?"))
+year_of_birth = 2023 - user_age
+
+# Calculate their age
+age_in_2049 = 2049 - year_of_birth
+
+# Output their age in 2049
+print(f"In 2049 you will be {age_in_2049} years old")
+
