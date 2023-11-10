4cc16ada8142adfcac8e9b719ab246b665432b46
Emma Xu <emmazihanxu@gmail.com>
2023-11-10 09:27:57 -0800

    completed sim score.py

---
+++ Activities/sfus_best_sim_score.py
@@ -0,0 +1,57 @@
+# SFU's Best
+# Emma
+# 10 November 2023
+
+# Load data from .csv file
+# Read it in a meaningful way
+# Link our similairty score algo to the data
+
+# Open the file
+with open("./data.csv") as f:
+    # Read the first line of data
+    
+    f.readline()
+
+# Create a "profile" for someone that shows their favourite places at SFU
+# Favourite places at SFU
+profile = [
+    "Bubble World",
+    "Chef Hung",
+    "Uncle Fatih's",
+    "Guadalupe (MBC)",
+    "Steve's Poke Bar"
+]
+
+# Initialize out top similarity score and their name
+top_sim_score = 0
+top_sim_name = ""
+
+with open("./data.csv") as f:
+    # Throw away the header line
+    header = f.readline()
+
+    # For every line of data in the file (string)
+    for line in f:
+        # Convert the line of data into a list
+        current_likes = line.split(",")
+
+        # Initialize the CURRENT sim score
+        # Store the current person's name
+        current_sim_score = 0
+        current_name = current_likes[1]
+
+        # sim score algo
+        for item in profile:
+            if item in current_likes:
+                current_sim_score += 1
+
+        # print the current sim score
+        print(f"{current_name} - Score: {current_sim_score}")
+
+        # Update the top score if this is the highest
+        if current_sim_score > top_sim_score:
+            top_sim_score = current_sim_score
+            top_sim_name = current_name
+
+print("TOP SIMILAR PERSON!")
+print(f"{top_sim_name} - Score: {top_sim_score}")
