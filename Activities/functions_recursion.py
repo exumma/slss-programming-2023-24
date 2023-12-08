e03785c6642995d52f9d0eba399d46a47eb57672
Emma Xu <emmazihanxu@gmail.com>
2023-12-08 14:19:15 -0800

    finished functions_recursion.py

--- Activities/functions_recursion.py
+++ Activities/functions_recursion.py
@@ -2,6 +2,8 @@
 # Emma
 # 6 December 2023
 
+import time
+
 def factorial(num: int) -> int:
     """returns the result of the number's factorial using recursion
     
@@ -24,4 +26,26 @@ def fib(n:int) -> int:
     elif n > 2:
         return fib(n - 1) + fib(n - 2)
 
-print(fib(1), fib(2), fib(8))
+def fib_itr(n: int) -> int:
+    """Returns the nth Fibonacci number.
+    Calculates this iteratively."""
+    last_num, num, result = 0, 1, 0
+
+    for _ in range(n-1):
+        result = last_num + num
+
+        num, last_num = result, num
+
+    return result
+
+time_initial = time.perf_counter()
+fib_itr(20)
+time_final = time.perf_counter()
+
+print(f"Iterative: {time_final - time_initial}")
+
+time_initial = time.perf_counter()
+fib(20)
+time_final = time.perf_counter()
+
+print(f"Recursive: {time_final - time_initial}")
