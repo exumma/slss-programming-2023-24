60c20b4f1841253a527cc5caf90ae87654d1971a
Emma Xu <emmazihanxu@gmail.com>
2023-12-13 10:33:22 -0800

    updated file

---
+++ Images/images_examples.py
@@ -0,0 +1,13 @@
+# Images and Python
+# Emma
+# 11 December 2023
+
+from PIL import Image
+
+# Recall that we can open up files in Python
+with Image.open("./Images/kid-green.jpg") as im:
+    # get the pixel information from the top left most pixel
+    pixel = im.getpixel((0,0))
+
+    # print the pixel information
+    print(pixel)
